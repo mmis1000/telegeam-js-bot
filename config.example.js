1448a4ffe458c578a338090df04daf3aa3a92da9
Mmis1000 <mmis1000@yahoo.com.tw>
2017-11-19 00:04:30 +0800

    Add option to configure container memory limit

--- config.example.js
+++ config.example.js
@@ -1,4 +1,7 @@
 module.exports = {
     token: '000000000:AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA',
-    image_name: 'mmis1000/lang-runner:0.1.2'
+    image_name: 'mmis1000/lang-runner:0.1.2',
+    engineOpts: {
+        memory: 256
+    }
 }
