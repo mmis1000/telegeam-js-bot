be516964047a692d309e18187a5591ae952dd5bc
Mmis1000 <mmis1000@yahoo.com.tw>
2019-04-24 23:46:55 +0800

    update

--- config.example.js
+++ config.example.js
@@ -1,6 +1,6 @@
 module.exports = {
     token: '000000000:AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA',
-    image_name: 'mmis1000/lang-runner:0.1.3',
+    image_name: 'mmis1000/lang-runner:0.1.3.1',
     engineOpts: {
         memory: 256
     }
