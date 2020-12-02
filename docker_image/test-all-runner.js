10278fc6adf85a58b31b22a76ce7dc9b9b47b024
Mmis1000 <mmis1000@yahoo.com.tw>
2020-12-02 14:46:53 +0800

    More type fixes

--- docker_image/test-all-runner.js
+++ docker_image/test-all-runner.js
@@ -4,19 +4,19 @@ const child_process = require("child_process");
 const helper = require("./test-helper");
 
 fs.readdir('./test', function (err, names) {
-    var JSONs = names.filter(function (name) {
+    var JSONFileNames = names.filter(function (name) {
         if (!name.match(/\.json$|\.templete$/)) console.error('found a file that neiter a templete and a json: ' + name)
         return /\.json/.test(name);
     })
-    JSONs = JSONs.map(function (name) {
-        var str = fs.readFileSync(path.resolve('./test', name));
+    var JSONTexts = JSONFileNames.map(function (name) {
+        var str = fs.readFileSync(path.resolve('./test', name), { encoding: 'utf-8' });
         var parsed = JSON.parse(str);
         parsed.user = 'debian';
         return JSON.stringify(parsed) + '\n';
     })
-    JSONs = JSONs.join('')
+    var JSONs = JSONTexts.join('')
     
-    console.log('test using image ' + require("../config.js").image_name);
+    console.log('test using image ' +  (process.argv[2] ?? require("../config.js").image_name));
     
     var child = child_process.spawn('docker', ['run', '-i', '--rm', require("../config.js").image_name, 'node', '/app/executer.js'])
     helper.hookStdout(child.stdout);
--- docker_image/test-helper.js
+++ docker_image/test-helper.js
@@ -1,6 +1,10 @@
 const readline = require('readline');
 
 module.exports = {
+    /**
+     * 
+     * @param {import('stream').Readable} stdout 
+     */
     hookStdout: function (stdout) {
         const rl = readline.createInterface({
             input: stdout
@@ -13,6 +17,7 @@ module.exports = {
                 Object.keys(parsedCommnad).forEach(function (key) {
                     console.log('  ' + (key + '        ').slice(0, 8) + ': ' + 
                         ('' + parsedCommnad[key] || ' ').split(/\r?\n/g).filter((i)=>!!i).map(function (line) {
+                            // @ts-ignore
                             return line.match(/.{1,70}/g).join('\r\n            ')
                         }).join('\r\n            ')
                     );
