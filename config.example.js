8383824230526764388d3499a3129950e13c3e1e
Mmis1000 <mmis1000@yahoo.com.tw>
2022-10-04 13:48:33 +0800

    update

--- config.example.js
+++ config.example.js
@@ -1,6 +1,6 @@
 module.exports = {
     token: '000000000:AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA',
-    image_name: 'mmis1000/lang-runner:0.1.5.1',
+    image_name: 'mmis1000/lang-runner:0.1.5.2',
     saves: {
         sessions: './saves/sessions',
         questDrafts: './saves/quest-drafts',
